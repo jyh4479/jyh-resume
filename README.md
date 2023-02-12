88c194f34f314dff33a7cb016db259abe4eba239
jyh4479 <jyh.dev@gmail.com>
2023-02-12 15:30:18 +0900

    [INIT] 프로젝트 생성 및 기본설정
    - path alias 설정 추가

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# jyh-resume
-React, Javascript, css 학습 및 간단한 이력서를 위한 레포
+React, javascript, css 학습 및 간단한 이력서를 위한 repo
+
+최근 사용하는 패턴이나 협업하기 좋은 코드가 무엇이지 고민하기 위한 repo
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "@", replacement: path.resolve(__dirname, "src")
+            },
+            {
+                find: "@components",
+                replacement: path.resolve(__dirname, "src/components"),
+            },
+            {
+                find: "@hooks",
+                replacement: path.resolve(__dirname, "src/hooks"),
+            },
+            {
+                find: "@stores",
+                replacement: path.resolve(__dirname, "src/stores"),
+            },
+        ]
+    }
+})
