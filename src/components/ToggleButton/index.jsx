8e736ab9359ed3d861e8917e00085b16996886bc
jyh4479 <jyh.dev@gmail.com>
2023-02-13 21:32:38 +0900

    [FEAT] Theme button
    테마 변경 버튼 구현

--- src/components/ToggleButton/index.jsx
+++ src/components/ToggleButton/index.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import styled from "@emotion/styled";
 
 const ToggleButton = (props) => {
 
@@ -12,8 +13,40 @@ const ToggleButton = (props) => {
     }
 
     return (
-        <input type="checkbox" onClick={check}/>
+        <ButtonBox>
+            <InputBox type="checkbox" onClick={check}/>
+            <ButtonBall checked={isTrue}/>
+        </ButtonBox>
     )
 }
 
 export default ToggleButton;
+
+const ButtonBox = styled.div`
+  position: relative;
+  width: 80px;
+  height: 40px;
+  border-radius: 80px;
+  border: 3px solid #888888;
+`
+
+const ButtonBall = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: ${props => props.checked ? "translate(120%, -50%)" : "translate(5%, -50%)"};
+  transition: 0.5s ease transform;
+  width: 35px;
+  height: 35px;
+  border-radius: 100px;
+  background-color: #646cff;
+`
+
+const InputBox = styled.input`
+  position: absolute;
+  width: 80px;
+  height: 40px;
+  border-radius: 80px;
+
+  opacity: 0;
+  z-index: 9999;
+`
