9e783e0fb4b0bcbc7890e91e579039539b801dce
jyh4479 <jyh.dev@gmail.com>
2023-02-20 21:14:34 +0900

    [ADD] Cursor event component

---
+++ src/containers/TitleTextContainer/CursorEvent/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "@emotion/styled";
+
+const CursorEvent = (props) => {
+
+    
+    return <CursorEventStyle/>
+}
+
+export default CursorEvent;
+
+const CursorEventStyle = styled.div`
+    
+`
